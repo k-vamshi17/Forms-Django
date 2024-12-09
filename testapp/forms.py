60b474d88034411afae1b4842c77a5b60f308ffe
Kuncha Vamshi Reddy <vamshireddy594@gmail.com>
2024-12-09 22:53:49 +0530

    changes

--- testapp/forms.py
+++ testapp/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+
+
+
+
+
 class StudentForms(forms.Form):
     name=forms.CharField()
+    rollno=forms.IntegerField()
     marks=forms.IntegerField()
     
--- testapp/views.py
+++ testapp/views.py
@@ -6,6 +6,17 @@ from django.shortcuts import render
 
 from testapp.forms import StudentForms
 def student_form_view(request):
+    submitted=False
+    sname=''
     form=StudentForms()
-    my_dict={'form':form}
-    return render(request,'testapp/input.html',my_dict)
+    if request.method=='POST':
+        form = StudentForms(request.POST)
+        if form.is_valid():
+            print('Form Validation sucess')
+            print('Name:',form.cleaned_data['name'])
+            print('Roll No:',form.cleaned_data['rollno'])
+            print('Marks:',form.cleaned_data['marks'])
+            sname=form.cleaned_data['name']
+            submitted=True
+    form=StudentForms()
+    return render(request,'testapp/input.html',{'form':form,'submitted':submitted,'sname':sname})
